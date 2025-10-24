004f63d85c0c8e8ba0b2a2fea8fe64bb671b1a59
Oliver Bock <146384680+obock1@users.noreply.github.com>
2025-10-24 11:27:03 -0500

    HTTPS Submission

---
+++ CMSC 23206 PKI Lab - Oliver Bock/website/https_server.py
@@ -0,0 +1,13 @@
+import http.server
+import ssl
+
+server_address = ('localhost', 8443)
+httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket(httpd.socket,
+                                server_side=True,
+                                certfile='cert.pem',
+                                keyfile='key.pem',
+                                ssl_version=ssl.PROTOCOL_TLS)
+
+httpd.serve_forever()
